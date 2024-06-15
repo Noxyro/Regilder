d1604fb0603dcea9c66a97b9fe23ed9020d754a0
Noxyro <noxyro@gmail.com>
2024-06-15 23:59:20 +0200

    Updated code to implement latest CH version and for minimum functionality, display the upcoming next 10 guilds in the pre-made list. Also added the extracted static hero data from the original game an added it as JSON data into its own file, accessible by the rest of the code now.

--- js/regilder.js
+++ js/regilder.js
@@ -1,75 +1,14 @@
 let LIB_PAKO = window.pako;
 let HASH_ZLIB = "7a990d405d2c6fb93aa8fbb0ec1a3b23";
-
-let HERO_NAMES=[
-  "Cid, the Helpful Adventurer",
-  "Treebeast",
-  "Ivan, the Drunken Brawler",
-  "Brittany, Beach Princess",
-  "The Wandering Fisherman",
-  "Betty Clicker",
-  "The Masked Samurai",
-  "Leon",
-  "The Great Forest Seer",
-  "Alexa, Assassin",
-  "Natalia, Ice Apprentice",
-  "Mercedes, Duchess of Blades",
-  "Bobby, Bounty Hunter",
-  "Broyle Lindeoven, Fire Mage",
-  "Sir George II, King's Guard",
-  "King Midas",
-  "Referi Jerator, Ice Wizard",
-  "Abaddon",
-  "Ma Zhu",
-  "Amenhotep",
-  "Beastlord",
-  "Athena, Goddess of War",
-  "Aphrodite, Goddess of Love",
-  "Shinatobe, Wind Deity",
-  "Grant, The General",
-  "Frostleaf",
-  "Dread Knight",
-  "Atlas",
-  "Terra"
-  ];
-
-let HERO_NAMES_SHORT=[
-  "Cid",
-  "Treebeast",
-  "Ivan",
-  "Brittany",
-  "Fisherman",
-  "Betty",
-  "Samurai",
-  "Leon",
-  "Forest Seer",
-  "Alexa",
-  "Natalia",
-  "Mercedes",
-  "Bobby",
-  "Broyle",
-  "Sir George",
-  "King Midas",
-  "Referi",
-  "Abaddon",
-  "Ma Zhu",
-  "Amenhotep",
-  "Beastlord",
-  "Athena",
-  "Aphrodite",
-  "Shinatobe",
-  "Grant",
-  "Frostleaf",
-  "Dread Knight",
-  "Atlas",
-  "Terra"
-];
+let HASH_DEFLATE = "7e8bb5a89f2842ac4af01b3b7e228592";
 
 let MESSAGE_STRINGS = {
   "error": {
     "system": {
-      0: "Missing message element in document.",
-      1: "Missing text input element in document.",
+      0: "Missing element in document.",
+      1: "Missing message element in document.",
+      2: "Missing text input element in document.",
+      3: "Missing loading element in document."
     },
     "read": {
       0: "Input is empty.",
@@ -78,16 +17,30 @@ let MESSAGE_STRINGS = {
       3: "Unable to process input.",
       4: "Unable to process input.",
       5: "Not a savegame.",
-      6: "Unsupported savegame."
+      6: "Unsupported savegame.",
+      7: "Error in hero data."
+    },
+    "data": {
+      0: "Invalid data. Data is null."
+    }
+  },
+  "generic": {
+    "savedata": {
+      0: "Savedata validated."
     }
   }
 };
 
-let data = null;
-let debug = false;
+let saveData = null;
+let heroData = null;
+let debug = true;
 
+let displayStates = {};
+
+let loaderElement = document.getElementById("loader");
 let saveDataMessageElement = document.getElementById("savedata-message");
 let saveDataTextInputElement = document.getElementById("savedata-input");
+
 
 let views;
 
@@ -134,41 +87,37 @@ function registerViews(views) {
 /*
  * ID to name getter functions
  */
-function getNameFromId(id, nameSource) {
-  if (id == null || id <= 0) {
-    return nameSource[0];
+function getHeroName(heroId, short = 0) {
+  if (heroData == null) { throw getErrorMessage("data", 0); }
+  if (heroId == null || heroId < 0) {
+    return heroData[0].name;
   }
 
-  return nameSource[id - 1];
+  let heroName = heroData[heroId - 1].name
+  if (!short) { return heroName; }
+
+  let searchComma = heroName.indexOf(", ");
+  if (searchComma) {
+    return heroName.substring(0, searchComma);
+  }
+
+  let searchSpaces = heroName.lastIndexOf(" ");
+  if (searchSpaces) {
+    return heroName.substring(searchSpaces, heroName.length);
+  }
+
+  return heroName;
 }
 
-function getHeroName(id) {
-  return getNameFromId(id, HERO_NAMES);
-}
-
-function getShortHeroName(id) {
-  return getNameFromId(id, HERO_NAMES_SHORT);
-}
-
-function getNamesFromIds(idArray, nameSource) {
+function getMultipleHeroNames(heroIdArray, short = 0) {
   let nameArray = [];
-  if (idArray == null || idArray.length === 0) {
-    return
-  }
+  if (heroIdArray == null || heroIdArray.length === 0) { return }
 
-  for (let i = 0; i < idArray.length; i++) {
-    nameArray[i] = getNameFromId(idArray[i], nameSource);
+  for (let i = 0; i < heroIdArray.length; i++) {
+    nameArray[i] = getHeroName(heroIdArray[i], short);
   }
 
   return nameArray;
-}
-
-function getHeroNames(idArray) {
-  return getNamesFromIds(idArray, HERO_NAMES);
-}
-
-function getShortHeroNames(idArray) {
-  return getNamesFromIds(idArray, HERO_NAMES_SHORT);
 }
 
 /*
@@ -199,17 +148,18 @@ function readSavegame(saveGame) {
     throw getErrorMessage("read", 0);
   }
 
-  if (!saveGame.startsWith(HASH_ZLIB)) {
-    if (debug) { console.log("No zlib hash found."); return; }
+  let hashAlgo = saveGame.startsWith(HASH_ZLIB) ? 1 : saveGame.startsWith(HASH_DEFLATE) ? 2 : 0
+  if (!hashAlgo) {
+    if (debug) { console.log("No compression algorithm hash found."); return; }
     throw getErrorMessage("read", 1);
   }
 
-  let rawData = saveGame.substr(32);
+  let rawData = saveGame.substring(32);
   if (debug) { console.log("Savegame without hash: " + rawData); }
 
   let decodedData;
   try {
-    decodedData = atob(rawData);
+    decodedData = atob(rawData); // Decode Base64
   } catch (error) {
     if (debug) { console.log("An error occurred during decoding:\n" + error.message); return; }
     throw getErrorMessage("read", 2);
@@ -219,7 +169,7 @@ function readSavegame(saveGame) {
 
   let decompressedData;
   try {
-    decompressedData = LIB_PAKO.inflate(decodedData, { to: 'string'});
+    decompressedData = hashAlgo === 2 ? LIB_PAKO.inflateRaw(decodedData, { to: 'string'}) : LIB_PAKO.inflate(decodedData, { to: 'string'});
   } catch (error) {
     if (debug) { console.log("An error occurred during decompression:\n" + error.message); return; }
     throw getErrorMessage("read", 3);
@@ -246,9 +196,15 @@ function readSavegame(saveGame) {
   let versionNumber = jsonData["version"];
   if (debug) { console.log("Patch: " + patchNumber + " | Version: " + versionNumber); }
 
-  if (patchNumber !== "1.0e11" || versionNumber !== 7) {
+  if ((!patchNumber.startsWith("1.0e11") && !patchNumber.startsWith("1.0e12")) || versionNumber !== 7) {
     if (debug) { console.log("The savegame patch version is not supported."); return; }
     throw getErrorMessage("read", 6);
+  }
+
+  heroData = JSON.parse(staticHeroData);
+  if (heroData == null) {
+    if (debug) { console.log("The hero data could not be loaded or read properly."); return; }
+    throw getErrorMessage("read", 7);
   }
 
   if (debug) { console.log("Savegame successfully read."); }
@@ -256,40 +212,56 @@ function readSavegame(saveGame) {
 }
 
 /*
+ * Gets the highest gold ever accumulated
+ */
+function getHighestGold() {
+  if (saveData == null) {
+    console.log("Invalid save data. Data is null.");
+    return;
+  }
+
+  return saveData["highestGold"]
+}
+
+/*
  * Gets the highest hero seen (first locked / not yet purchasable hero) of the savegame
  */
 function getHighestHeroSeen() {
-  if (data == null) {
-    console.log("Invalid data. Data is null.");
-    return;
+  if (saveData == null) {
+    console.log("Invalid save data. Data is null.");
+    return 0;
   }
 
-  let heroCollection = data["heroCollection"];
-  if (heroCollection == null) {
-    console.log("Invalid data. No heroCollection found.");
-    return;
+  if (heroData == null) {
+    console.log("Invalid hero data. Data is null.");
+    return 0;
   }
 
-  let heroes = heroCollection["heroes"];
-  if (heroes == null) {
-    console.log("Invalid data. No heroes found in heroCollection.");
-    return;
-  }
+  let highestGoldString = getHighestGold();
+  let highestGold = Number(highestGoldString.replace("e\d", "e+"))
+  if (debug) { console.log("Highest gold: " + highestGold); }
+  let highestHeroSeen = 0;
+  for (let i = 0; i < heroData.length; i++) {
+    let hero = heroData[i];
+    if (hero == null) { continue; }
 
-  if (debug) { console.log("Heroes: " + JSON.stringify(data["heroCollection"]["heroes"])); }
+    let baseCost = Number(hero.baseCost);
+    if (debug) { console.log("Hero: " + hero.name + " | Base cost: " + baseCost); }
 
-  for (let hero in heroes) {
-    if (debug) { console.log("Hero: " + JSON.stringify(heroes[hero])); }
-    if (heroes[hero]["locked"] === true) {
-      return heroes[hero]["id"];
+    if (highestGold < baseCost) {
+      highestHeroSeen = hero.id;
+      break;
     }
   }
-
-  return null;
+  return highestHeroSeen;
 }
 
 /*
  * Gets the next seed with given previous seed. Based on the original game formula.
+ * Original definition of Random.Rand:
+ *      public long Rand() { if (this.seed == 0L) throw new InvalidOperationException("Rand() called without a seed"); ++this.numUses; this.seed = this.seed * 16807L % (long) int.MaxValue; return this.seed; }
+ *
+ * Important part in short: this.seed = this.seed * 16807L % int.MaxValue; return this.seed;
  */
 function getNextSeed(seed) {
   return seed * 16807 % 2147483647;
@@ -297,21 +269,26 @@ function getNextSeed(seed) {
 
 /*
  * Gets the next gild with given seed and highest hero seen. Based on the original game formula.
+ * Original formula:
+ *    Call to Random.Range:         key = random.Range(2U, max);
+ *    Definition of Random.Range:   public float Range(float min, float max) { return (float) this.Rand() % (float) ((double) max - (double) min + 1.0) + min; }
+ *      the important in short:     return this.Rand() % (max - min + 1.0) + min;
+ *    Call to this.Rand() is equal to getNextSeed()
  */
 function getNextGild(seed, highestHeroSeen) {
-  return seed % (highestHeroSeen - 1) + 2;
+  return seed % (highestHeroSeen - 2 + 1) + 2;
 }
 
 /*
  * Calculates the next number of gilds. Based on the original game formula.
  */
 function calculateNextGilds(num) {
-  if (data == null) {
+  if (saveData == null) {
     console.log("Invalid data. Data is null.");
     return;
   }
 
-  let seed = data["epicHeroSeed"];
+  let seed = saveData["epicHeroSeed"];
   if (seed == null || seed === 0) {
     console.log("Invalid data. No epicHeroSeed found.");
     return;
@@ -335,7 +312,7 @@ function calculateNextGilds(num) {
     nextGilds[i] = gild;
   }
 
-  console.log("Next gilds: " + JSON.stringify(getShortHeroNames(nextGilds)));
+  console.log("Next gilds: " + JSON.stringify(getMultipleHeroNames(nextGilds)));
   return nextGilds;
 }
 
@@ -352,13 +329,36 @@ function switchView(view) {
 }
 
 function switchViewToRegilder() {
-  calculateNextGilds(10);
+  let nextGilds = calculateNextGilds(10);
+  if (debug) { console.log("Next gilds: " + nextGilds); }
   switchView("regilder");
+  for (i = 0; i < nextGilds.length; i++) {
+    let listEntry = document.getElementById("regilder-output-list-entry-" + i);
+    if (listEntry == null) {
+      listEntry = createElement("li", "regilder-output-list-entry-" + i,);
+      document.getElementById("regilder-output-list").appendChild(listEntry);
+    }
+
+    let heroName = getHeroName(nextGilds[i])
+    setElementInnerHTML(listEntry, "<img src=\"favicon.ico\" alt=\"" + nextGilds[i] + "\"/> - " + heroName)
+  }
 }
 
 /*
  * HTML helper functions
  */
+function setElementInnerHTML(element, innerHTML) {
+  element.innerHTML = innerHTML;
+}
+
+function setElementAttribute(element, attribute, value) {
+  if (element == null || attribute === "") {
+    return;
+  }
+
+  element.setAttribute(attribute, value);
+}
+
 function setElementStyleProperty(element, property, value) {
   if (element == null || element.style == null || property == null || property === "") {
     return;
@@ -371,49 +371,124 @@ function setElementDisplay(element, value) {
   setElementStyleProperty(element, "display", value);
 }
 
-function displayMessage(text, color) {
-  if (saveDataMessageElement == null) {
+function createElement(tagName, id = "", classList = [], innerHTML = "") {
+  let element = document.createElement(tagName)
+  classList.forEach((clazz) => { element.classList.add(clazz) });
+  element.innerHTML = innerHTML;
+
+  return element;
+}
+
+function removeElement(element) {
+  element.parentElement.removeChild(element)
+}
+
+function showElement(element) {
+  if (element == null) {
     console.log(getErrorMessage("system", 0));
     return;
   }
 
-  setElementDisplay(saveDataMessageElement, "block");
-  setElementStyleProperty(saveDataMessageElement, "color", color);
-  saveDataMessageElement.innerHTML = text;
+  console.log(JSON.stringify(displayStates));
+
+  if (!(element.id in displayStates) || displayStates[element.id] == null || displayStates[element.id] === "") {
+    element.style.removeProperty("display");
+  }
+
+  if (debug) { console.log("Showing element: " + element.id); }
+  if (displayStates[element.id] === "none") {
+    setElementDisplay(element, "inherit");
+  } else {
+    setElementDisplay(element, displayStates[element.id]);
+  }
+
+  delete displayStates[element.id];
 }
 
-function hideMessage() {
-  if (saveDataMessageElement == null) {
+function hideElement(element) {
+  if (element == null) {
     console.log(getErrorMessage("system", 0));
     return;
   }
 
-  setElementDisplay(saveDataMessageElement,"none");
-  saveDataMessageElement.innerHTML = "";
+  if (!(element.id in displayStates)) {
+    let displayProperty;
+    if (element.style == null) {
+      displayProperty = null;
+    } else {
+      if (element.style.getPropertyValue("display") == null || element.style.getPropertyValue("display") === "") {
+        displayProperty = window.getComputedStyle(element).getPropertyValue("display")
+      } else {
+        displayProperty = element.style.getPropertyValue("display");
+      }
+    }
+
+    displayStates[element.id] = displayProperty;
+  }
+
+  if (debug) { console.log("Hiding element: " + element.id); }
+  setElementDisplay(element, "none");
 }
 
-function processTextInput() {
-  hideMessage();
+function showMessage(messageElement, text, color) {
+  if (messageElement == null) {
+    console.log(getErrorMessage("system", 1));
+    return;
+  }
+
+  showElement(messageElement);
+  setElementStyleProperty(messageElement, "color", color);
+  messageElement.innerHTML = text;
+}
+
+function hideMessage(messageElement) {
+  if (messageElement == null) {
+    console.log(getErrorMessage("system", 1));
+    return;
+  }
+
+  hideElement(messageElement);
+  messageElement.innerHTML = "";
+}
+
+function showLoader() {
+  showElement(loaderElement);
+}
+
+function hideLoader() {
+  hideElement(loaderElement);
+}
+
+function processSavegameInput() {
+  hideMessage(saveDataMessageElement);
 
   if (saveDataTextInputElement == null) {
     if (debug) { console.log("No text input element found."); }
-    displayMessage(getErrorMessage("system", 1), "#00FF00");
+    showMessage(saveDataMessageElement, getErrorMessage("system", 2), "#00FF00");
     return;
   }
 
   let textInput = saveDataTextInputElement.value;
   if (debug) { console.log("Given input: " + textInput); }
 
-  let saveGame;
-  try {
-    saveGame = readSavegame(textInput);
-  } catch (e) {
-    displayMessage(e, "#FF0000");
-    return;
-  }
+  showElement(loaderElement);
 
-  displayMessage("Validated.", "#00FF00");
-  data = saveGame;
+  setTimeout(() => {
+    let saveGame;
+    try {
+      saveGame = readSavegame(textInput);
+    } catch (e) {
+      showMessage(saveDataMessageElement, e, "#FF0000");
+      return;
+    } finally {
+      hideElement(loaderElement);
+    }
 
-  setTimeout(switchViewToRegilder, 1000)
+    showMessage(saveDataMessageElement, getMessageString("generic", "savedata", 0), "#00FF00");
+    saveData = saveGame;
+
+    setTimeout(switchViewToRegilder, 2000); // 2000
+    setTimeout(showLoader, 1500); // 1500
+    setTimeout(hideLoader, 2100); // 2100
+  }, 500); // 500
 }
